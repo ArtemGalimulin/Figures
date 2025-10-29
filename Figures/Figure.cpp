ad8d6749a26cb8a7c3925c245262ea12b3b09f35
Артём Галимулин <artem_utro@mail.ru>
2025-10-29 21:14:44 +0300

    added Figure file

---
+++ Figures/Figure.cpp
@@ -0,0 +1,6 @@
+#include "Figure.h"
+
+class Figure {
+  void virtual area() const = 0;
+  void virtual perimeter() const = 0;
+};
--- Figures/Source.cpp
+++ Figures/Source.cpp
@@ -1,25 +1,23 @@
-class Figure {
-  double virtual size() const = 0;
-};
-
-class One_Dimension : protected Figure {
-  double virtual size() const = 0;
-};
-
-class Segment {
-  double st;
-  double end;
-};
-
-class Two_D {
-
-}
-
-class Three_Dimensions {
-
-};
-
-
-int main() {
-
-}
+//class Figure {
+//  double virtual size() const = 0;
+//};
+//
+//class One_Dimension : protected Figure {
+//  double virtual size() const = 0;
+//};
+//
+//class Segment {
+//  double st;
+//  double end;
+//};
+//
+//class Two_D {};
+//
+//class Three_Dimensions {
+//
+//};
+//
+//
+//int main() {
+//
+//}
